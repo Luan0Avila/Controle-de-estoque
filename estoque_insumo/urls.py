611761eccece56773cb1c96af7432304735cde6e
Luan0Avila <avila.almeida.luan@gmail.com>
2025-09-09 20:38:39 -0300

    começando a alterar os templates

--- estoque_insumo/urls.py
+++ estoque_insumo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'estoque_insumo'
+
 urlpatterns = [
-    path('', views.estoque_insumo_view, name='estoque_insumo'),
+    path('', views.estoque_insumo_view, name='home'),
 ]
